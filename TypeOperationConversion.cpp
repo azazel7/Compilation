e3ecc17b2291a42f1b6a507411bd98f692e3cd0f
magoa <mel@statler.world>
2014-12-05 14:53:45 +0000

    Complete matrix

--- TypeOperationConversion.cpp
+++ TypeOperationConversion.cpp
@@ -12,6 +12,7 @@ void TypeOperationConversion::initTable(void)
 	std::map<char, char> loutre;
 	std::map<char, std::map<char, char> > canard;
 
+	//ADD operation
 	loutre[POINTER_TYPE] = POINTER_TYPE;
 	loutre[INT_TYPE] = INT_TYPE;
 	loutre[FLOAT_TYPE] = INT_TYPE;
@@ -28,7 +29,52 @@ void TypeOperationConversion::initTable(void)
 	canard[POINTER_TYPE] = loutre;
 	loutre.clear();
 
-	canard[VOID_TYPE] = loutre;
 	loutre.clear();
 	table[OPERATION_ADD] = canard;
+
+	//SUB operation
+	loutre[POINTER_TYPE] = POINTER_TYPE;
+	loutre[INT_TYPE] = INT_TYPE;
+	loutre[FLOAT_TYPE] = INT_TYPE;
+	canard[INT_TYPE] = loutre;
+	loutre.clear();
+
+	loutre[INT_TYPE] = INT_TYPE;
+	loutre[FLOAT_TYPE] = FLOAT_TYPE;
+	canard[FLOAT_TYPE] = loutre;
+	loutre.clear();
+
+	loutre[POINTER_TYPE] = POINTER_TYPE;
+	loutre[INT_TYPE] = POINTER_TYPE;
+	canard[POINTER_TYPE] = loutre;
+	loutre.clear();
+
+	loutre.clear();
+	table[OPERATION_SUB] = canard;
+
+	//BY operation
+	loutre[INT_TYPE] = INT_TYPE;
+	loutre[FLOAT_TYPE] = INT_TYPE;
+	canard[INT_TYPE] = loutre;
+	loutre.clear();
+
+	loutre[INT_TYPE] = INT_TYPE;
+	loutre[FLOAT_TYPE] = FLOAT_TYPE;
+	canard[FLOAT_TYPE] = loutre;
+	loutre.clear();
+
+	table[OPERATION_BY] = canard;
+
+	//DIV operation
+	loutre[INT_TYPE] = INT_TYPE;
+	loutre[FLOAT_TYPE] = INT_TYPE;
+	canard[INT_TYPE] = loutre;
+	loutre.clear();
+
+	loutre[INT_TYPE] = INT_TYPE;
+	loutre[FLOAT_TYPE] = FLOAT_TYPE;
+	canard[FLOAT_TYPE] = loutre;
+	loutre.clear();
+
+	table[OPERATION_DIV] = canard;
 }
