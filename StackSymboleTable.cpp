f07975e8d2a60b3b43f2f86b89b7f7ce840125a2
magoa <mel@statler.world>
2014-12-28 10:27:49 +0100

    Add function to deal withlabel or location

--- StackSymboleTable.cpp
+++ StackSymboleTable.cpp
@@ -1,6 +1,8 @@
 #include "StackSymboleTable.hpp"
+#include <stdexcept>
 
 std::list<std::map<std::string, Type const*> > StackSymboleTable::stack;
+std::list<std::map<std::string, std::string> > StackSymboleTable::stackLocation;
 
 void StackSymboleTable::push(std::map<std::string, Type const*> table)
 {
@@ -10,6 +12,8 @@ void StackSymboleTable::pop(void)
 {
 	stack.pop_front();
 	//TODO think about delete Type* at the end
+	if(stackLocation.size() > 0)
+		stackLocation.pop_front();
 }
 Type const* StackSymboleTable::getSymbole(std::string name)
 {
@@ -17,4 +21,17 @@ Type const* StackSymboleTable::getSymbole(std::string name)
 		if(table.count(name) == 1)
 			return table[name];
 	return nullptr;
+}
+std::string StackSymboleTable::getLocation(std::string name)
+{
+	for(auto table : stackLocation)
+		if(table.count(name) == 1)
+			return table[name];
+	throw std::invalid_argument("Can't find symbole " + name + " in getLocation");
+}
+
+void StackSymboleTable::push(std::map<std::string, Type const*> table, std::map<std::string, std::string> location)
+{
+	stackLocation.push_front(location);
+	StackSymboleTable::push(table);
 }
--- StackSymboleTable.hpp
+++ StackSymboleTable.hpp
@@ -9,10 +9,13 @@ class Type;
 class StackSymboleTable
 {
 	static std::list<std::map<std::string, Type const*> > stack;
+	static std::list<std::map<std::string, std::string> > stackLocation;
 	public:
 	static void push(std::map<std::string, Type const*> table);
+	static void push(std::map<std::string, Type const*> table, std::map<std::string, std::string> location);
 	static void pop(void);
 	static Type const* getSymbole(std::string name);
+	static std::string getLocation(std::string name);
 	
 };
 #endif
